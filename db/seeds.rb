c7313d404623fe472266ccc61e1b67bf2c9d01a0
Joseph Lee <jhjlee702@gmail.com>
2025-02-03 20:47:03 -0700

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,8 @@ Transaction.destroy_all
 
 joseph = User.create(name: "Joseph Lee", email: "jhjlee702@gmail.com", password_digest: "1234")
 
-retirement_portfolio = Portfolio.create(user_id: joseph.id, name: "retirement" )
+retirement_portfolio = Portfolio.create(user_id: joseph.id, name: "retirement")
 
 vanguard_index_fund = Asset.create(portfolio_id: retirement_portfolio.id, symbol: "VTSAX", name: "Vanguard Total Stock Market Index Fund Admiral Shares", quantity: 100)
 
-vg_purchase = Transaction.create(asset_id: vanguard_index_fund.id, trade_type: "Purchase", amount: 100, price: 145.39, executed_at: "2024-02-03 14:00:00 UTC"
-)
+vg_purchase = Transaction.create(asset_id: vanguard_index_fund.id, trade_type: "Purchase", amount: 100, price: 145.39, executed_at: "2024-02-03 14:00:00 UTC")
