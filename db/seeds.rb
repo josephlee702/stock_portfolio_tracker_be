606321a8766b4582320a4f9d31b9af2c375aff28
Joseph Lee <jhjlee702@gmail.com>
2025-02-03 20:46:22 -0700

    create some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+Portfolio.destroy_all
+Asset.destroy_all
+Transaction.destroy_all
+
+joseph = User.create(name: "Joseph Lee", email: "jhjlee702@gmail.com", password_digest: "1234")
+
+retirement_portfolio = Portfolio.create(user_id: joseph.id, name: "retirement" )
+
+vanguard_index_fund = Asset.create(portfolio_id: retirement_portfolio.id, symbol: "VTSAX", name: "Vanguard Total Stock Market Index Fund Admiral Shares", quantity: 100)
+
+vg_purchase = Transaction.create(asset_id: vanguard_index_fund.id, trade_type: "Purchase", amount: 100, price: 145.39, executed_at: "2024-02-03 14:00:00 UTC"
+)
