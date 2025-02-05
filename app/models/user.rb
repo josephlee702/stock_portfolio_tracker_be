27babbfb69ef53f5d386e25eebd61ccfe09e523c
Joseph Lee <jhjlee702@gmail.com>
2025-02-04 18:17:28 -0700

    add validates and specifications for fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :portfolios, dependent: :destroy
 
-  #bcrypt user auth
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> {new_record? || !password.nil? }
+
+  #hashes passwords using bcrypt
   has_secure_password
 end
