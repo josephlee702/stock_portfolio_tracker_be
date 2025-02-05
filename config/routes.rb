44a411d25553f4092c708815e649b23bd10ff592
Joseph Lee <jhjlee702@gmail.com>
2025-02-05 16:24:05 -0700

    add users show route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: :index
+      resources :users, only: [:index, :show]
       post "signup", to: "authentication#signup"
       post "login", to: "authentication#login"
     end
