1e0951928b72756ae505068d30afe4eb6f4bbab8
Joseph Lee <jhjlee702@gmail.com>
2025-02-05 16:41:39 -0700

    set up API service to call in controller

---
+++ app/services/market_data_service.rb
@@ -0,0 +1,24 @@
+require "faraday"
+require "json"
+
+class MarketDataService
+  BASE_URL = ""
+
+  def initialize
+    @conn = Faraday.new(url: BASE_URL) do |faraday|
+      faraday.request :url_encoded
+      faraday.response :json, content_type: /\bjson$/
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def fetch_asset_price(symbol)
+    response = @conn.get("/v1/assets/#{symbol}") do |req|
+      req.headers["Authorization"] = "Bearer #{ ENV["API_KEY"] }"
+    end
+
+    return JSON.parse(response.body) if response.success?
+
+    { error: "Failed to fetch asset price" }
+  end
+end
