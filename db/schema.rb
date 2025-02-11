fced6ccc300e16a68d613ac4143008c2a5717238
Joseph Lee <jhjlee702@gmail.com>
2025-02-10 18:42:51 -0700

    disregard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_031901) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_11_013609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_031901) do
     t.datetime "executed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "portfolio_id", null: false
+    t.string "transaction_type"
     t.index ["asset_id"], name: "index_transactions_on_asset_id"
+    t.index ["portfolio_id"], name: "index_transactions_on_portfolio_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +57,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_031901) do
   add_foreign_key "assets", "portfolios"
   add_foreign_key "portfolios", "users"
   add_foreign_key "transactions", "assets"
+  add_foreign_key "transactions", "portfolios"
 end
