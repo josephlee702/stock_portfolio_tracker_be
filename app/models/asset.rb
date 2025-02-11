7af694dc31a31301dec8dfcac4dca4ad79cc4908
Joseph Lee <jhjlee702@gmail.com>
2025-02-10 17:03:58 -0700

    add Asset class validations

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,8 @@
 class Asset < ApplicationRecord
   belongs_to :portfolio
   has_many :transactions, dependent: :destroy
+
+  validates :symbol, presence: true
+  validates :name, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
