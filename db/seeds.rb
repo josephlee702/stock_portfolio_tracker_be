2edb2807d2edfa37098e00019844ccfb8f93229c
Joseph Lee <jhjlee702@gmail.com>
2025-02-11 17:44:04 -0700

    edit asset seeds to have market_prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@ joseph = User.create(name: "Joseph Lee", email: "jhjlee702@gmail.com", password:
 
 retirement_portfolio = Portfolio.create(user_id: joseph.id, name: "retirement")
 
-vanguard_index_fund = Asset.create(portfolio_id: retirement_portfolio.id, symbol: "VTSAX", name: "Vanguard Total Stock Market Index Fund Admiral Shares", quantity: 100)
+vanguard_index_fund = Asset.create(portfolio_id: retirement_portfolio.id, symbol: "VTSAX", name: "Vanguard Total Stock Market Index Fund Admiral Shares", quantity: 100, market_price: 500.0)
 
 vg_purchase = Transaction.create(asset_id: vanguard_index_fund.id, trade_type: "Purchase", amount: 100, price: 145.39, executed_at: "2024-02-03 14:00:00 UTC")
 
-Asset.create(portfolio_id: joseph.id, symbol: "BTC", name: "Bitcoin", quantity: 100)
+Asset.create(portfolio_id: retirement_portfolio.id, symbol: "BTC", name: "Bitcoin", quantity: 100, market_price: nil)
