9476841a78c1769a7c63de2560e4616f32757ab4
Joseph Lee <jhjlee702@gmail.com>
2025-02-11 17:44:17 -0700

    add market_price validation

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,5 +5,6 @@ class Asset < ApplicationRecord
   validates :symbol, presence: true
   validates :name, presence: true
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
-  validates :market_price, numericality: { greater_than_or_equal_to: 0 }
+  #we allow nil here just in case market_price isn't available
+  validates :market_price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
