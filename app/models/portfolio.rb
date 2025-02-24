054a3784b0585685b57c17835719cd0d74fe69a3
Joseph Lee <jhjlee702@gmail.com>
2025-02-23 18:58:57 -0700

    create function to calculate total_market_value

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,9 @@ class Portfolio < ApplicationRecord
   belongs_to :user
   has_many :assets, dependent: :destroy
   has_many :transactions, dependent: :destroy
+
+  def total_market_value
+    market_value = assets.sum { |asset| asset.market_price * asset.quantity }
+    market_value.toFixed(2)
+  end
 end
