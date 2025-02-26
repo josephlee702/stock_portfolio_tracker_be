9febb3381d92bbe7a04d2b0206eaabc4ab6d0d12
Joseph Lee <jhjlee702@gmail.com>
2025-02-26 16:56:05 -0700

    Devise stuff added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  has_many :portfolios, dependent: :destroy
+        extend Devise::Models
+
+        # Include default devise modules.
+        devise :database_authenticatable, :registerable,
+               :recoverable, :rememberable, :validatable,
+               :confirmable, :trackable, :omniauthable
+      
+        include DeviseTokenAuth::Concerns::User
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, if: -> {new_record? || !password.nil? }
