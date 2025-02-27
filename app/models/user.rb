f7b62e00377dd6fc34262474b887c50b5e698074
Joseph Lee <jhjlee702@gmail.com>
2025-02-26 18:08:38 -0700

    adjust user model for devise stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-        extend Devise::Models
+       extend Devise::Models
+       devise :database_authenticatable, :registerable,
+              :recoverable, :rememberable, :validatable
 
-        # Include default devise modules.
-        devise :database_authenticatable, :registerable,
-               :recoverable, :rememberable, :validatable
-      
-        include DeviseTokenAuth::Concerns::User
+       include DeviseTokenAuth::Concerns::User
 
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }, if: -> {new_record? || !password.nil? }
+       # validates :email, presence: true, uniqueness: true
+       # validates :password, presence: true, length: { minimum: 6 }, if: -> {new_record? || !password.nil? }
 end
