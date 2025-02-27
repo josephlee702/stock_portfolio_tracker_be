a4829b24ce0e9fa3f1a96f31a3bb41bc5e20fede
Joseph Lee <jhjlee702@gmail.com>
2025-02-26 17:36:24 -0700

    add devise auth route under api v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
+
+      mount_devise_token_auth_for 'User', at: "auth"
+      
       post "signup", to: "authentication#signup"
-      post "login", to: "authentication#login"
+      post "login", to: "sessions#login"
+      delete "logout", to: "sessions#destroy"
 
       #/api/v1/portfolios(/:id)
       resources :portfolios, only: [:index, :show, :create, :update, :destroy] do
