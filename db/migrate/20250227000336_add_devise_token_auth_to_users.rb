ffdabf090b2171f8fb2d016b4e80bccd0405c4e3
Joseph Lee <jhjlee702@gmail.com>
2025-02-26 17:37:15 -0700

    migration for devise token to users

---
+++ db/migrate/20250227000336_add_devise_token_auth_to_users.rb
@@ -0,0 +1,21 @@
+class AddDeviseTokenAuthToUsers < ActiveRecord::Migration[7.1]
+  def change
+    change_table(:users) do |t|
+      ## Required
+      t.string :provider, null: false, default: "email"
+      t.string :uid, null: false, default: ""
+
+      ## Trackable (optional)
+      t.integer :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
+
+      ## Tokens
+      t.text :tokens
+    end
+
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
