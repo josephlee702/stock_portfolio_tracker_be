a39ebd4b47a5b70fe25f850d881c253b2a438ad1
Joseph Lee <jhjlee702@gmail.com>
2025-03-01 17:14:51 -0700

    readd one to many relationship with portfolios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+       has_many :portfolios, dependent: :destroy
+
        extend Devise::Models
        devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
