a4e6bc5f9432463987200a7273d45ed011bd01fb
Joseph Lee <jhjlee702@gmail.com>
2025-03-01 17:07:31 -0700

    add email field to permit the field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation])
   end
 end
