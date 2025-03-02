deee791b80d759c154cf1cced08cb67f47ef6d5e
Joseph Lee <jhjlee702@gmail.com>
2025-03-01 17:06:01 -0700

    remove custom JWT token authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,6 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  def authenticate_user
-    header = request.headers["Authorization"]
-    token = header.split(" ").last if header.present?
-    begin
-      decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base, true, algorithm: "HS256")
-      user_id = decoded_token[0]["user_id"]
-      @current_user = User.find(user_id)
-    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-      render json: { error: "Unauthorized" }, status: 401
-    end
-  end
-
   def current_user
     @current_user
   end
