974391e24b66c1a97e28e640f89c7087c08debf4
Joseph Lee <jhjlee702@gmail.com>
2025-03-02 18:41:25 -0700

    expose the tokens for log-in

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3001'  # this allows the backend to communicate with the front end, bypassing default CORS setting
     resource '*',
       headers: :any,
-      expose: ['Authorization'],  # Allow headers like Authorization for JWT
+      expose: ['access-token', 'client', 'uid', 'expiry'],  #expose the tokens
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
